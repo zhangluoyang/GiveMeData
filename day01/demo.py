398762e845f34d8e544b5227aa1544fbe5803883
张洛阳 <55058629@qq.com>
2018-04-26 00:48:30 +0800

    百度图片下载刘亦菲性感

--- day01/demo.py
+++ day01/demo.py
@@ -10,7 +10,7 @@ url = 'http://www.qiushibaike.com/hot/page/' + str(page)
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent' : user_agent }
 try:
-    request = urllib2.Request(url,headers = headers)
+    request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
     content = response.read().decode('utf-8')
     # 找出对应的文本数据 以及评论数目
